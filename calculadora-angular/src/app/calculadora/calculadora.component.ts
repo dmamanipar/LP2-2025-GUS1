d1b2ac4890852fe6cea1cf765240a30cf3b0719a
noetipo <noe.tipo@gmail.com>
2025-05-22 08:17:04 -0500

    operacion basica calc

--- calculadora-angular/src/app/calculadora/calculadora.component.ts
+++ calculadora-angular/src/app/calculadora/calculadora.component.ts
@@ -4,9 +4,24 @@ import { Component } from '@angular/core';
   selector: 'app-calculadora',
   standalone:true,
   imports: [],
+  /*template: `
+    <p>calculadora works DMO!</p>
+    `,*/
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.css'
 })
 export class CalculadoraComponent {
+
+  num1:number=0;
+  num2:number=0;
+  result:number=0;
+
+  getNum1(e:any){
+    console.log(e);
+  }
+
+  getNum2(e:any){
+    console.log(e);
+  }
 
 }
