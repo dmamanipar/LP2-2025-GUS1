1a3f5c405e9e5dd560e223d434f52f071c299e9c
noetipo <noe.tipo@gmail.com>
2025-05-22 09:07:52 -0500

    operaciones basicas P2

--- calculadora-angular/src/app/calculadora/calculadora.component.ts
+++ calculadora-angular/src/app/calculadora/calculadora.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {evaluate} from 'mathjs';
 
 @Component({
   selector: 'app-calculadora',
@@ -11,17 +12,24 @@ import { Component } from '@angular/core';
   styleUrl: './calculadora.component.css'
 })
 export class CalculadoraComponent {
-
   num1:number=0;
   num2:number=0;
-  result:number=0;
+  result:String="";
+  oper:string=""
 
   getNum1(e:any){
+    this.num1=e.target.value;
     console.log(e);
   }
 
   getNum2(e:any){
     console.log(e);
+    this.num2=e.target.value;
+  }
+
+  operar(oper:string){
+    this.oper=oper;
+    this.result=evaluate(this.num1+this.oper+this.num2);
   }
 
 }
