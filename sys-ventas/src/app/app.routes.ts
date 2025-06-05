bc78034e2cc789d75dbc23392794b6b748f10911
noetipo <noe.tipo@gmail.com>
2025-06-05 09:53:24 -0500

    basica

--- sys-ventas/src/app/app.routes.ts
+++ sys-ventas/src/app/app.routes.ts
@@ -3,6 +3,12 @@ import { Routes } from '@angular/router';
 import {LayoutComponent} from './paginas/layout/layout.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo:'main' , pathMatch: 'full' },
+  { path: '', redirectTo:'pages' , pathMatch: 'full' },
   { path: 'main', component: LayoutComponent, },
+  { path: 'pages',
+    component: LayoutComponent,
+    loadChildren:()=>import('./paginas/pages.routes').then(x=>x.pagesRoutes)
+  },
+
+
 ];
