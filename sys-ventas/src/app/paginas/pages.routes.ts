f33e53783e01e3aeaf24421b4041b2c1e8c6f6ba
noetipo <noe.tipo@gmail.com>
2025-06-12 10:11:03 -0500

    Spring Security + JWT Angular

--- sys-ventas/src/app/paginas/pages.routes.ts
+++ sys-ventas/src/app/paginas/pages.routes.ts
@@ -5,15 +5,18 @@ import {MainMarcaComponent} from './main-marca/main-marca.component';
 import {FormMarcaComponent} from './main-marca/form-marca/form-marca.component';
 import {MainProductoComponent} from './main-producto/main-producto.component';
 import {FormxProductoComponent} from './main-producto/formx-producto/formx-producto.component';
+import {Not403Component} from './not403/not403.component';
+import {certGuard} from '../guard/cert.guard';
 
 export const pagesRoutes: Routes = [
-  { path: 'dashboard', component: DashboardComponent,  },
+  { path: 'dashboard', component: DashboardComponent, /*canActivate: [certGuard]*/ },
   { path: 'mattable', component: MatejemploComponent },
   { path: 'marca', component: MainMarcaComponent,
     children:[
       { path: 'new', component: FormMarcaComponent },
       { path: 'edit/:id', component: FormMarcaComponent },
-    ]
+    ],
+    /*canActivate: [certGuard]*/
   },
 
   {
@@ -22,9 +25,9 @@ export const pagesRoutes: Routes = [
     children: [
       { path: 'new', component: FormxProductoComponent },
       { path: 'edit/:id', component: FormxProductoComponent },
-    ],
+    ], /*canActivate: [certGuard]*/
   },
-
+  {path:'not-403', component: Not403Component}
   /*{ path: 'categoria', component: MainCategoriaComponent , },
   //{ path: 'categoria', component: MainCategoriaComponent },
   {
